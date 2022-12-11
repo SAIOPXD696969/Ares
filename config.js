d34004b978c14ca1630f1f29b2ceaa983c0b8cd8
AAYAN <amit2840327@gmail.com>
2022-12-11 08:51:05 +0000

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const data = {
-    "token": "OTUxMjAxNjY4NjY4NzM5NjU2.GiB7hs.XknMFNN2AIpnzRVDgSCdStKu8IWKkkdxv-HZLU" || process.env.token,
+    "token": "" || process.env.token,
     "mongo": "mongodb+srv://Test_Bot:8851020767@test.idqc2xz.mongodb.net/?retryWrites=true&w=majority" || process.env.mongo,
  
 }
